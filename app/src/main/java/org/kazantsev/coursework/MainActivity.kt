3a30d9dca137b41531939b56902b8d81a70cec7d
Egor Kazantsev <3gork4z4ncev@gmail.com>
2021-05-03 15:33:05 +0500

    Added navigation between fragments through BottomNavigationView and NavigationComponent (the fragments are still empty)

--- app/src/main/java/org/kazantsev/coursework/MainActivity.kt
+++ app/src/main/java/org/kazantsev/coursework/MainActivity.kt
@@ -2,10 +2,27 @@ package org.kazantsev.coursework
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupBottomNavView()
+    }
+
+    private fun setupBottomNavView() {
+        // find by id
+        val bottomNavView = findViewById<BottomNavigationView>(R.id.bottomNavView)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+
+        // set navigation controller
+        val navController = navHostFragment.navController
+        bottomNavView.setupWithNavController(navController)
     }
 }
---
+++ app/src/main/java/org/kazantsev/coursework/fragments/CartFragment.kt
@@ -0,0 +1,20 @@
+package org.kazantsev.coursework.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import org.kazantsev.coursework.R
+
+class CartFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_cart, container, false)
+
+        return view
+    }
+}
